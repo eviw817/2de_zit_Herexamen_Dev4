39d8d9709c0f3bb1096c9e136fd449f390f2d110
eviw817 <116573280+eviw817@users.noreply.github.com>
2023-08-27 22:12:17 +0200

    last update

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>IMDFlix</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+  <div id="netflix">
+  <?php include_once("nav.inc.php"); ?>
+  
+  <div class="collection">
+    
+    <a href="details.php?id=1" class="collection__item" style="background-image: url('https://www.manners.nl/wp-content/uploads/2020/04/Tiger-King-Netflix-1.jpg')">
+    </a>
+   
+  </div>
+  
+</div>
+
+</body>
+</html>
